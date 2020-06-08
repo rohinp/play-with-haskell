0fdfc334efebf8208197c0e7a65728d4cd7d1a7d
Rohin Patel <rohin.patel@ing.com>
2020-06-08 17:37:42 +0200

    Crypto assignment

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,74 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ show $ (rotate 3 "ABCDEFG")
+{-
+data Trivial =
+  Trivial'
+
+instance Eq Trivial where
+  Trivial' == Trivial' = True
+
+--  data Identity a = Identity a
+--instance Eq (Identity a) where
+--(==) (Identity v) (Identity v') = v == v'
+
+data TisAnInteger =
+      TisAn Integer
+
+instance Eq (TisAnInteger) where
+     (==) (TisAn a) (TisAn a') = a == a'
+
+data TwoIntegers = Two Integer Integer
+instance Eq TwoIntegers where
+    (==) (Two a b) (Two a' b') = (a == a') && (b == b')
+
+data StringOrInt = TisAnInt Int | TisAString String
+instance Eq StringOrInt where
+    (==) (TisAnInt a) (TisAnInt b) = (a == b)
+    (==) (TisAString a) (TisAString b) = (a == b)
+    (==) _ _ = False
+
+data Pair a = Pair a a
+instance Eq a => Eq (Pair a) where
+    (==) (Pair a a') (Pair b b') = (a == b) && (a' == b')
+
+
+data Tuple a b = Tuple a b
+instance (Eq a ,Eq b) => Eq (Tuple a b) where
+    (==) (Tuple a a') (Tuple b b') = (a == b) && (a' == b')
+
+
+data EitherOr a b = Hello a | Goodbye b
+instance (Eq a ,Eq b) => Eq (EitherOr a b) where
+    (==) (Hello a ) (Hello a') = (a == a')
+    (==) (Goodbye b ) (Goodbye b') = (b == b')
+    (==) _ _ = False 
+
+bindExp :: p -> [Char]
+bindExp x = let x = 10; y = 5 in
+    "the integer was: " ++ show x ++ " and y was: " ++ show y
+
+addOneIfOdd :: Integral p => p -> p
+addOneIfOdd n = case odd n of
+    True -> (\x -> x + 1) n
+    False -> n
+
+isItTwo :: Integer -> Bool
+isItTwo _ = False
+isItTwo 2 = True
+
+functionC :: Ord p => p -> p -> p
+functionC x y = case (x > y) of
+    True -> x 
+    False -> y
+
+nums :: (Ord a, Num a, Num p) => a -> p
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        EQ -> 0
+
+
+-}
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,45 @@
-module Lib ( someFunc ) where
+module Lib where 
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Safe
+import Data.Maybe
+import Data.Bifoldable
 
-oneMore:: Int -> Int
-oneMore x = x * 12
+rotate :: Int -> String -> String
+rotate _ [] = []
+rotate n chars@(x:xs)
+    | n == 0 = chars
+    | n < 0 = error "Less than usual"
+    | n > (length chars) = error "Too big"
+    | otherwise = rotate (n-1) (xs ++ [x]) 
 
 
+prop_rotate :: Int -> String -> Bool
+prop_rotate k str = rotate (l - m) (rotate m str) == str
+                        where l = length str
+                              m = if l == 0 then 0 else k `mod` l
 
+
+makeKey :: Int -> [(Char, Char)]
+makeKey n = zip alphabet (rotate n alphabet)
+    where alphabet = ['A'..'Z']
+
+-- 
+
+lookupRec :: Char -> [(Char, Char)] -> Char
+lookupRec c [] = c
+lookupRec c ((k,v):kvs) 
+    | k == c = v
+    | otherwise = lookupRec c kvs
+
+lookUp :: Char -> [(Char, Char)] -> Char
+lookUp c xs = fromMaybe c $ headMay [ b | (a,b) <- xs, a == c] 
+
+lookUp1 :: Char -> [(Char, Char)] -> Char
+lookUp1 c xs
+    | null v = c
+    | otherwise = head v
+    where 
+        v = [ b | (a,b) <- xs, a == c]
+
+prop_lookUp :: Char -> [(Char, Char)] -> Bool
+prop_lookUp c xs = lookUp c xs == lookupRec c xs
