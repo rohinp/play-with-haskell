bb098a4a37af9d9f059f1f2f567b51b31af62c7e
hamid <hamid@fastforward.ai>
2020-06-08 18:49:46 +0200

    Jun 8th

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,8 @@ module Lib where
 import Safe
 import Data.Maybe
 import Data.Bifoldable
+import Data.Char
+import Data.List
 
 rotate :: Int -> String -> String
 rotate _ [] = []
@@ -18,10 +20,10 @@ prop_rotate k str = rotate (l - m) (rotate m str) == str
                         where l = length str
                               m = if l == 0 then 0 else k `mod` l
 
+alphabet = ['A'..'Z']
 
 makeKey :: Int -> [(Char, Char)]
 makeKey n = zip alphabet (rotate n alphabet)
-    where alphabet = ['A'..'Z']
 
 -- 
 
@@ -43,3 +45,21 @@ lookUp1 c xs
 
 prop_lookUp :: Char -> [(Char, Char)] -> Bool
 prop_lookUp c xs = lookUp c xs == lookupRec c xs
+
+encipher :: Int -> Char -> Char
+encipher n char = lookUp char $ makeKey n
+
+
+normalize1 :: String -> String
+normalize1 xs = [toUpper x | x <- xs , isDigit x || isAlpha x]
+
+normalizeN :: String -> String
+normalizeN [] = []
+normalizeN (x1 : xs) = normalize x1 ++ normalizeN xs
+    where
+        normalize x 
+            | isAlpha x || isDigit x= [toUpper x]
+            | otherwise = []
+
+normalizeYXZ :: String -> String
+normalizeYXZ x = [toUpper y | y <- x, isAlpha y && isDigit y]
